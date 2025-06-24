5aff766bc3b36655bc9ce3e25214b813cf43e000
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-06-24 20:48:32 +0700

    Refactor database seeders & update timezone to Asia/Ho_Chi_Minh

--- database/seeders/FileSeeder.php
+++ database/seeders/FileSeeder.php
@@ -14,33 +14,21 @@ class FileSeeder extends Seeder
      */
     public function run(): void
     {
-        // Tao 1000 ban ghi bang file
-        $files = collect([]);
-        for ($i = 0; $i < 1000; $i++) {
-            $files->push([
-                'filename' => 'fileame-' . $i . '.jpeg',
-                'deadline' => now(), // Lay thoi gian hien tai
-                'status' => 1,
-                'priority' => 1,
-                'user_id' => rand(2, 100),
-                'synchronize' => 0,
-                'created_at' => now(),
-                'updated_at' => now()
-            ]);
-        }
-        // Debug trong Laravel,
-        try {
-            // Tao 1 yeu cau giao dich voi CSDL
-            DB::beginTransaction();
-            // Khi thao tac cua yeu cau thanh cong thi moi cap nhat du lieu
+        DB::transaction(function () {
+            $files = collect([]);
+            for ($i = 0; $i < 1000; $i++) {
+                $files->push([
+                    'filename' => 'fileame-' . $i . '.jpeg',
+                    'deadline' => now()->addDay(),
+                    'status' => 1,
+                    'priority' => 1,
+                    'user_id' => rand(2, 100),
+                    'synchronize' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]);
+            }
             DB::table('files')->insert($files->toArray());
-            // Tien hanh xu ly du lieu
-            DB::commit();
-        } catch (\Exception $e) {
-            // Neu thao tac giao dich vs CSDL that bai
-            // Tien hanh rollback ve du lieu cu
-            DB::rollBack();
-            dd($e);
-        }
+        });
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,34 +14,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // DB::table('ten bang')-><cac lenh query>(); // de ket noi toi bang CSDL
-        /**
-         * Query builder: thao tac truc tiep toi bang CSDL.
-         * Uu diem: toc do nhanh vi thao tac truc tiep toi CSDL.
-         * Nhuoc diem: xu ly logic khong manh.
-         */
-        // Tao 1 admin va 99 editor
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('12345678'),
-            'role' => 1,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        for ($i = 1; $i < 100; $i++) {
+        DB::transaction(function () {
+            $editors = collect([]);
+            for ($i = 0; $i < 100; $i++) {
+                $editors->push([
+                    'name' => fake()->name(),
+                    'email' => fake()->unique()->safeEmail(),
+                    'password' => Hash::make('12345678'),
+                    'role' => 0,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]);
+            }
+            DB::table('users')->insert($editors->toArray());
+
             DB::table('users')->insert([
-                'name' => 'editor-' . $i,
-                'email' => 'editorEmail' . $i . '@gmail.com',
-                /**
-                 * Ma hoa mat khau duoi dang SHA
-                 * Khoa bi mat la gia tri cua APP_KEY trong env
-                 */
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
                 'password' => Hash::make('12345678'),
-                'role' => 0,
+                'role' => 1,
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
-        }
+        });
     }
 }
